89a436d7fbbb9e42f6bae05b795a38f0662b8e6b
Rishitha-0703 <rishithadontham807@gmail.com>
2026-08-30 15:10:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function closeMessage() {
         notification.remove();
     }
 }
-const API = "https://kisaanconnect-aeny.onrender.com";
+const API = "http://127.0.0.1:5000";
 
 
 // =====================================================
